b80a524feed1abb6aff5ec497a5e802bfdaaab12
IDGS-904-20000804 <ivanornelas.school@gmail.com>
2023-07-06 20:52:17 -0600

    feat(database structure and data): corrected structure and added sample data

--- database/main-query.sql
+++ database/main-query.sql
@@ -1,146 +1,203 @@
+USE master;
+GO
+
+IF EXISTS (SELECT * FROM sys.databases WHERE name = N'ClayTrack') DROP DATABASE ClayTrack;
+
 CREATE DATABASE ClayTrack;
-GO;
+GO
+
 USE ClayTrack;
-GO;
-CREATE TABLE unidad_medida (
-  id_unidad_medida INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  descripcion VARCHAR(255) NOT NULL,
-  estatus BOOLEAN DEFAULT TRUE NOT NULL,
-  fecha_creacion DATETIME NOT NULL DEFAULT NOW(),
-  fecha_actualizacion DATETIME NOT NULL DEFAULT NOW()
+GO
+
+CREATE TABLE CatUnitMeasure (
+  idCatUnitMeasure INT IDENTITY(1,1) PRIMARY KEY,
+  description VARCHAR(255) NOT NULL,
+  status BIT DEFAULT 1 NOT NULL,
+  creationDate DATETIME NOT NULL DEFAULT GETDATE(),
+  updateDate DATETIME NOT NULL DEFAULT GETDATE()
 );
-CREATE TABLE materia_prima (
-  id_materia_prima INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(255) NOT NULL,
-  estatus BOOLEAN DEFAULT TRUE NOT NULL,
-  fecha_creacion DATETIME NOT NULL DEFAULT NOW(),
-  fecha_actualizacion DATETIME NOT NULL DEFAULT NOW(),
-  fk_unidad_medida INT NOT NULL
+GO
+
+CREATE TABLE CatRawMaterial (
+  idCatRawMaterial INT IDENTITY(1,1) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  status BIT DEFAULT 1 NOT NULL,
+  creationDate DATETIME NOT NULL DEFAULT GETDATE(),
+  updateDate DATETIME NOT NULL DEFAULT GETDATE(),
+  fkCatUnitMeasure INT NOT NULL
 );
-CREATE TABLE roles (
-  id_rol INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(255) NOT NULL,
-  estatus BOOLEAN DEFAULT TRUE NOT NULL,
-  fecha_creacion DATETIME NOT NULL DEFAULT NOW(),
-  fecha_actualizacion DATETIME NOT NULL DEFAULT NOW()
+GO
+
+CREATE TABLE CatRole (
+  idCatRole INT IDENTITY(1,1) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  status BIT DEFAULT 1 NOT NULL,
+  creationDate DATETIME NOT NULL DEFAULT GETDATE(),
+  updateDate DATETIME NOT NULL DEFAULT GETDATE()
 );
-CREATE TABLE usuario (
-  id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  correo VARCHAR(255) NOT NULL,
-  contrasenia VARCHAR(255) NOT NULL,
-  estatus BOOLEAN DEFAULT TRUE NOT NULL,
-  fecha_creacion DATETIME NOT NULL DEFAULT NOW(),
-  fecha_actualizacion DATETIME NOT NULL DEFAULT NOW()
+GO
+
+CREATE TABLE CatUser (
+  idCatUser INT IDENTITY(1,1) PRIMARY KEY,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  status BIT DEFAULT 1 NOT NULL,
+  creationDate DATETIME NOT NULL DEFAULT GETDATE(),
+  updateDate DATETIME NOT NULL DEFAULT GETDATE()
 );
-CREATE TABLE rol_usuario (
-  id_rol_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  fk_usuario INT NOT NULL,
-  fk_rol INT NOT NULL
+GO
+
+CREATE TABLE DetailRoleUser (
+  idDetailRoleUser INT IDENTITY(1,1) PRIMARY KEY,
+  fkCatUser INT NOT NULL,
+  fkCatRole INT NOT NULL
 );
-CREATE TABLE persona (
-  id_persona INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(255) NOT NULL,
-  apaterno VARCHAR(255) NOT NULL,
-  amaterno VARCHAR(255),
-  telefono VARCHAR(255) NOT NULL,
-  codigo_postal INT NOT NULL,
-  numero_exterior VARCHAR(255) NOT NULL,
-  numero_interior VARCHAR(255),
-  calle VARCHAR(255) NOT NULL,
-  colonia VARCHAR(255) NOT NULL,
-  estatus BOOLEAN DEFAULT TRUE NOT NULL,
-  fecha_creacion DATETIME NOT NULL DEFAULT NOW(),
-  fecha_actualizacion DATETIME NOT NULL DEFAULT NOW()
+GO
+
+CREATE TABLE CatPerson (
+  idCatPerson INT IDENTITY(1,1) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  lastName VARCHAR(255) NOT NULL,
+  middleName VARCHAR(255),
+  phone VARCHAR(255) NOT NULL,
+  postalCode INT NOT NULL,
+  streetNumber VARCHAR(255) NOT NULL,
+  apartmentNumber VARCHAR(255),
+  street VARCHAR(255) NOT NULL,
+  neighborhood VARCHAR(255) NOT NULL,
+  status BIT DEFAULT 1 NOT NULL,
+  creationDate DATETIME NOT NULL DEFAULT GETDATE(),
+  updateDate DATETIME NOT NULL DEFAULT GETDATE()
 );
-CREATE TABLE cliente (
-  id_cliente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  fk_persona INT NOT NULL,
-  fk_usuario INT NOT NULL
+GO
+
+CREATE TABLE CatClient (
+  idCatClient INT IDENTITY(1,1) PRIMARY KEY,
+  fkCatPerson INT NOT NULL,
+  fkCatUser INT NOT NULL
 );
-CREATE TABLE proveedor (
-  id_proveedor INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  correo VARCHAR(255) NOT NULL,
-  fk_persona INT NOT NULL
+GO
+
+CREATE TABLE CatSupplier (
+  idCatSupplier INT IDENTITY(1,1) PRIMARY KEY,
+  email VARCHAR(255) NOT NULL,
+  fkCatPerson INT NOT NULL
 );
-CREATE TABLE empleado (
-  id_empleado INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  fk_persona INT NOT NULL,
-  fk_usuario INT NOT NULL
+GO
+
+CREATE TABLE CatEmployee (
+  idCatEmployee INT IDENTITY(1,1) PRIMARY KEY,
+  fkCatPerson INT NOT NULL,
+  fkCatUser INT NOT NULL
 );
-CREATE TABLE compra (
-  id_compra INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+GO
+
+CREATE TABLE CatPurchase (
+  idCatPurchase INT IDENTITY(1,1) PRIMARY KEY,
   total FLOAT NOT NULL,
-  fecha_creacion DATETIME NOT NULL DEFAULT NOW(),
-  fecha_actualizacion DATETIME NOT NULL DEFAULT NOW(),
-  fk_proveedor INT NOT NULL,
-  fk_empleado INT NOT NULL
+  creationDate DATETIME NOT NULL DEFAULT GETDATE(),
+  updateDate DATETIME NOT NULL DEFAULT GETDATE(),
+  fkCatSupplier INT NOT NULL,
+  fkCatEmployee INT NOT NULL
 );
-CREATE TABLE detalle_compra (
-  id_detalle_compra INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  cantidad FLOAT NOT NULL,
-  precio FLOAT NOT NULL,
-  fk_almacen INT NOT NULL,
-  fk_compra INT NOT NULL
+GO
+
+CREATE TABLE DetailPurchase (
+  idDetailPurchase INT IDENTITY(1,1) PRIMARY KEY,
+  quantity FLOAT NOT NULL,
+  price FLOAT NOT NULL,
+  fkCatWarehouse INT NOT NULL,
+  fkCatPurchase INT NOT NULL
 );
-CREATE TABLE almacen (
-  id_almacen INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  cantidad INT NOT NULL,
-  caducidad DATETIME NOT NULL,
-  estatus BOOLEAN DEFAULT TRUE NOT NULL,
-  fecha_creacion DATETIME NOT NULL DEFAULT NOW(),
-  fecha_actualizacion DATETIME NOT NULL DEFAULT NOW(),
-  fk_materia_prima INT NOT NULL
+GO
+
+CREATE TABLE CatWarehouse (
+  idCatWarehouse INT IDENTITY(1,1) PRIMARY KEY,
+  quantity INT NOT NULL,
+  status BIT DEFAULT 1 NOT NULL,
+  creationDate DATETIME NOT NULL DEFAULT GETDATE(),
+  updateDate DATETIME NOT NULL DEFAULT GETDATE(),
+  fkCatRawMaterial INT NOT NULL
 );
-CREATE TABLE receta (
-  id_receta INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(255) NOT NULL,
-  precio FLOAT NOT NULL,
-  ruta_imagen VARCHAR(255) NOT NULL,
-  estatus BOOLEAN DEFAULT TRUE NOT NULL,
-  fecha_creacion DATETIME NOT NULL DEFAULT NOW(),
-  fecha_actualizacion DATETIME NOT NULL DEFAULT NOW()
+GO
+
+CREATE TABLE CatRecipe (
+  idCatRecipe INT IDENTITY(1,1) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  price FLOAT NOT NULL,
+  imagePath VARCHAR(255) NOT NULL,
+  status BIT DEFAULT 1 NOT NULL,
+  creationDate DATETIME NOT NULL DEFAULT GETDATE(),
+  updateDate DATETIME NOT NULL DEFAULT GETDATE()
 );
-CREATE TABLE detalle_materia_prima_receta (
-  id_detalle_materia_prima_receta INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  fk_receta INT NOT NULL,
-  cantidad FLOAT NOT NULL,
-  fk_materia_prima INT NOT NULL
+GO
+
+CREATE TABLE DetailRecipeRawMaterial (
+  idDetailRecipeRawMaterial INT IDENTITY(1,1) PRIMARY KEY,
+  fkCatRecipe INT NOT NULL,
+  quantity FLOAT NOT NULL,
+  fkCatRawMaterial INT NOT NULL
 );
-CREATE TABLE detalle_almacen_stock (
-  id_detalle_almacen_stock INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  cantidad FLOAT NOT NULL,
-  fk_almacen INT NOT NULL,
-  fk_stock INT NOT NULL
+GO
+
+CREATE TABLE CatStock (
+  idCatStock INT IDENTITY(1,1) PRIMARY KEY,
+  quantity INT NOT NULL,
+  price FLOAT NOT NULL,
+  status BIT DEFAULT 1 NOT NULL,
+  creationDate DATETIME NOT NULL DEFAULT GETDATE(),
+  updateDate DATETIME NOT NULL DEFAULT GETDATE(),
+  fkCatRecipe INT NOT NULL
 );
-CREATE TABLE stock (
-  id_stock INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  caducidad VARCHAR(255) NOT NULL,
-  cantidad INT NOT NULL,
-  precio FLOAT NOT NULL,
-  estatus BOOLEAN DEFAULT TRUE NOT NULL,
-  fecha_creacion DATETIME NOT NULL DEFAULT NOW(),
-  fecha_actualizacion DATETIME NOT NULL DEFAULT NOW(),
-  fk_receta INT NOT NULL
-);
-CREATE TABLE venta (
-  id_venta INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+GO
+
+CREATE TABLE CatSale (
+  idCatSale INT IDENTITY(1,1) PRIMARY KEY,
   total FLOAT NOT NULL,
-  fecha_creacion DATETIME NOT NULL DEFAULT NOW(),
-  fecha_actualizacion DATETIME NOT NULL DEFAULT NOW(),
-  fk_cliente INT NOT NULL
+  creationDate DATETIME NOT NULL DEFAULT GETDATE(),
+  updateDate DATETIME NOT NULL DEFAULT GETDATE(),
+  fkCatClient INT NOT NULL
 );
-CREATE TABLE detalle_venta (
-  id_detalle_venta INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  cantidad FLOAT NOT NULL,
-  precio FLOAT NOT NULL,
-  fk_stock INT NOT NULL,
-  fk_venta INT NOT NULL
+GO
+
+CREATE TABLE DetailSale (
+  idDetailSale INT IDENTITY(1,1) PRIMARY KEY,
+  quantity FLOAT NOT NULL,
+  price FLOAT NOT NULL,
+  fkCatStock INT NOT NULL,
+  fkCatSale INT NOT NULL
 );
-CREATE TABLE envio (
-  id_envio INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  entregado BOOLEAN NOT NULL DEFAULT FALSE,
-  fecha_creacion DATETIME NOT NULL DEFAULT NOW(),
-  fecha_actualizacion DATETIME NOT NULL DEFAULT NOW(),
-  fk_venta INT NOT NULL,
-  fk_empleado INT
+GO
+
+CREATE TABLE CatShipment (
+  idCatShipment INT IDENTITY(1,1) PRIMARY KEY,
+  delivered BIT NOT NULL DEFAULT 0,
+  creationDate DATETIME NOT NULL DEFAULT GETDATE(),
+  updateDate DATETIME NOT NULL DEFAULT GETDATE(),
+  fkCatSale INT NOT NULL,
+  fkCatEmployee INT
 );
+GO
+
+INSERT INTO CatUnitMeasure (description) VALUES ('Pieces'), ('Kilograms'), ('Liters');
+INSERT INTO CatRawMaterial (name, fkCatUnitMeasure) VALUES ('Clay', 1), ('Glaze', 2), ('Pigment', 3);
+INSERT INTO CatRole (name) VALUES ('Administrator'), ('Manager'), ('Employee');
+INSERT INTO CatUser (email, password) VALUES ('admin@craftedclay.com', 'admin123'), ('manager@craftedclay.com', 'manager123'), ('employee@craftedclay.com', 'employee123');
+INSERT INTO DetailRoleUser (fkCatUser, fkCatRole) VALUES (1, 1), (2, 2), (3, 3);
+INSERT INTO CatPerson (name, lastName, phone, postalCode, streetNumber, street, neighborhood) VALUES
+('John', 'Doe', '1234567890', 12345, '1A', 'Main Street', 'Downtown'),
+('Jane', 'Smith', '9876543210', 54321, '2B', 'Park Avenue', 'Suburb');
+INSERT INTO CatClient (fkCatPerson, fkCatUser) VALUES (1, 1), (2, 2);
+INSERT INTO CatSupplier (email, fkCatPerson) VALUES ('supplier1@craftedclay.com', 1), ('supplier2@craftedclay.com', 2);
+INSERT INTO CatEmployee (fkCatPerson, fkCatUser) VALUES (1, 3), (2, 3), (3, 3), (4, 3), (5, 3);
+INSERT INTO CatPurchase (total, fkCatSupplier, fkCatEmployee) VALUES (100.00, 1, 1), (200.00, 2, 2), (150.00, 1, 3);
+INSERT INTO DetailPurchase (quantity, price, fkCatWarehouse, fkCatPurchase) VALUES (10, 5.00, 1, 1), (5, 8.00, 2, 1), (8, 3.50, 3, 2);
+INSERT INTO CatWarehouse (quantity, fkCatRawMaterial) VALUES (20, 1), (30, 2), (15, 3);
+INSERT INTO CatRecipe (name, price, imagePath) VALUES ('Mug', 15.00, 'mug.jpg'), ('Bowl', 10.00, 'bowl.jpg');
+INSERT INTO DetailRecipeRawMaterial (quantity, fkCatRecipe, fkCatRawMaterial) VALUES (1.5, 1, 1), (0.75, 1, 2), (0.5, 2, 1);
+INSERT INTO CatStock (quantity, price, fkCatRecipe) VALUES (10, 15.00, 1), (20, 10.00, 2), (15, 12.00, 3);
+INSERT INTO CatSale (total, fkCatClient) VALUES (50.00, 1), (100.00, 2);
+INSERT INTO DetailSale (quantity, price, fkCatStock, fkCatSale) VALUES (2, 15.00, 1, 1), (1, 10.00, 2, 1), (3, 12.00, 3, 2);
+INSERT INTO CatShipment (delivered, fkCatSale, fkCatEmployee) VALUES (1, 1, 3), (0, 2, 4);
+UPDATE CatWarehouse SET quantity = quantity - 2 WHERE idCatWarehouse = 1;
+UPDATE CatStock SET quantity = quantity - 2 WHERE idCatStock = 1;
