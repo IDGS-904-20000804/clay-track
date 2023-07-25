eb57cf58ab312e1596d59bbe4f46aa66293649e8
Paula Valencia <pau_doly@hotmail.com>
2023-07-24 21:19:34 -0600

    Servicios con base de datos

---
+++ services/API_ClayTrack/DataBase/ClayTrackDbContext.cs
@@ -0,0 +1,187 @@
+﻿using API_ClayTrack.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_ClayTrack.DataBase
+{
+    public class ClayTrackDbContext : IdentityDbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //CatPurchase fkCatSupplier and fkCatEmployee
+            modelBuilder.Entity<CatPurchase>()
+                .HasOne(p => p.Supplier)
+                .WithMany()
+                .HasForeignKey(p => p.fkCatSupplier)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<CatPurchase>()
+                .HasOne(p => p.Employee)
+                .WithMany()
+                .HasForeignKey(p => p.fkCatEmployee)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            //CatPurchase fkCatSale and fkCatEmployee
+            modelBuilder.Entity<CatShipment>()
+                .HasOne(p => p.Sale)
+                .WithMany()
+                .HasForeignKey(p => p.fkCatSale)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<CatShipment>()
+                .HasOne(p => p.Employee)
+                .WithMany()
+                .HasForeignKey(p => p.fkCatEmployee)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            //CatPerson status, creationDate and updateDate
+            modelBuilder.Entity<CatPerson>()
+                .Property(b => b.status)
+                .HasDefaultValue(true);
+
+            modelBuilder.Entity<CatPerson>()
+                .Property(c => c.creationDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<CatPerson>()
+                .Property(c => c.updateDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            //CatPurchase creationDate and updateDate
+            modelBuilder.Entity<CatPurchase>()
+                .Property(c => c.creationDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<CatPurchase>()
+                .Property(c => c.updateDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            //CatRawMaterial status, creationDate and updateDate
+            modelBuilder.Entity<CatRawMaterial>()
+                .Property(b => b.status)
+                .HasDefaultValue(true);
+
+            modelBuilder.Entity<CatRawMaterial>()
+                .Property(c => c.creationDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<CatRawMaterial>()
+                .Property(c => c.updateDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            //CatRecipe status, creationDate and updateDate
+            modelBuilder.Entity<CatRecipe>()
+                .Property(b => b.status)
+                .HasDefaultValue(true);
+
+            modelBuilder.Entity<CatRecipe>()
+                .Property(c => c.creationDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<CatRecipe>()
+                .Property(c => c.updateDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            //CatRole status, creationDate and updateDate
+            modelBuilder.Entity<CatRole>()
+                .Property(b => b.status)
+                .HasDefaultValue(true);
+
+            modelBuilder.Entity<CatRole>()
+                .Property(c => c.creationDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<CatRole>()
+                .Property(c => c.updateDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            //CatSale status, creationDate and updateDate
+            modelBuilder.Entity<CatSale>()
+                .Property(c => c.creationDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<CatSale>()
+                .Property(c => c.updateDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            //CatShipment status, creationDate and updateDate
+            modelBuilder.Entity<CatShipment>()
+                .Property(b => b.delivered)
+                .HasDefaultValue(true);
+
+            modelBuilder.Entity<CatShipment>()
+                .Property(c => c.creationDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<CatShipment>()
+                .Property(c => c.updateDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            //CatStock status, creationDate and updateDate
+            modelBuilder.Entity<CatStock>()
+                .Property(b => b.status)
+                .HasDefaultValue(true);
+
+            modelBuilder.Entity<CatStock>()
+                .Property(c => c.creationDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<CatStock>()
+                .Property(c => c.updateDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            //CatUnitMeasure status, creationDate and updateDate
+            modelBuilder.Entity<CatUnitMeasure>()
+                .Property(b => b.status)
+                .HasDefaultValue(true);
+
+            modelBuilder.Entity<CatUnitMeasure>()
+                .Property(c => c.creationDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<CatUnitMeasure>()
+                .Property(c => c.updateDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            //CatUser status, creationDate and updateDate
+            modelBuilder.Entity<CatUser>()
+                .Property(b => b.status)
+                .HasDefaultValue(true);
+
+            modelBuilder.Entity<CatUser>()
+                .Property(c => c.creationDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<CatUser>()
+                .Property(c => c.updateDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public ClayTrackDbContext(DbContextOptions dbContextOptions): base(dbContextOptions) 
+        {
+            
+        }
+
+        public DbSet<CatClient> CatClient { get; set; }
+        public DbSet<CatEmployee> CatEmployee { get; set; }
+        public DbSet<CatPerson> CatPerson { get; set; }
+        public DbSet<CatRawMaterial> CatRawMaterial { get; set; }
+        public DbSet<CatRecipe> CatRecipe { get; set; }
+        public DbSet<CatRole> CatRole { get; set; }
+        public DbSet<CatSale> CatSale { get; set; }
+        public DbSet<CatShipment> CatShipment { get; set; }
+        public DbSet<CatStock> CatStock { get; set; }
+        public DbSet<CatSupplier> CatSupplier { get; set; }
+        public DbSet<CatUnitMeasure> CatUnitMeasure { get; set; }
+        public DbSet<CatUser> CatUser { get; set; }
+        public DbSet<CatWarehouse> CatWarehouse { get; set; }
+        public DbSet<DetailPurchase> DetailPurchase { get; set; }
+        public DbSet<DetailRecipeRawMaterial> DetailRecipeRawMaterial { get; set; }
+        public DbSet<DetailRoleUser> DetailRoleUser { get; set; }
+        public DbSet<DetailSale> DetailSale { get; set; }
+        public DbSet<CatPurchase> CatPurchase { get; set; }
+
+    }
+}
