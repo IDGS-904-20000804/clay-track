398cd9f47810a986ab288125bd8e87dcb12d7779
Paula Valencia <pau_doly@hotmail.com>
2023-08-06 00:05:43 -0600

    Id Roles que se borraron

--- services/API_ClayTrack/Token/TokenRepository.cs
+++ services/API_ClayTrack/Token/TokenRepository.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using API_ClayTrack.DataBase;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -9,23 +11,37 @@ namespace API_ClayTrack.Repositories.Token
     public class TokenRepository : ITokenRepository
     {
         private readonly IConfiguration configuration;
+        private readonly ClayTrackDbContext dbContext;
 
-        public TokenRepository(IConfiguration configuration)
+        public TokenRepository(IConfiguration configuration, ClayTrackDbContext dbContext)
         {
             this.configuration = configuration;
+            this.dbContext = dbContext;
         }
 
 
         public string CreateJWTToken(IdentityUser user, List<string> roles)
         {
             // Create claims
-            var claims = new List<Claim>();
-
-            claims.Add(new Claim(ClaimTypes.Email, user.Email));
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+            };
 
             foreach (var role in roles)
             {
                 claims.Add(new Claim(ClaimTypes.Role, role));
+
+                if (role == "Client") // Assuming "Client" is the role for clients
+                {
+                    var clientId = GetClientIdFromDatabase(user.Id);
+                    claims.Add(new Claim("ClientId", clientId.ToString()));
+                }
+                else if (role == "Employee") // Assuming "Employee" is the role for employees
+                {
+                    var employeeId = GetEmployeeIdFromDatabase(user.Id);
+                    claims.Add(new Claim("EmployeeId", employeeId.ToString()));
+                }
             }
 
 
@@ -40,6 +56,18 @@ namespace API_ClayTrack.Repositories.Token
                 signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private int GetClientIdFromDatabase(string userId)
+        {
+            var client = dbContext.CatClient.FirstOrDefault(c => c.fkUser == userId);
+            return client?.idCatClient ?? 0;
+        }
+
+        private int GetEmployeeIdFromDatabase(string userId)
+        {
+            var employee = dbContext.CatEmployee.FirstOrDefault(e => e.fkUser == userId);
+            return employee?.idCatEmployee ?? 0;
         }
     }
 }
