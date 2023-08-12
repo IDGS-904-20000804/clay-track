8a9f6fe7af46eb22c02224a2f3d21c8912bc57dc
Paula Valencia <pau_doly@hotmail.com>
2023-08-12 09:23:53 -0600

    Analityc getAll

--- services/API_ClayTrack/Controllers/PurchaseController.cs
+++ services/API_ClayTrack/Controllers/PurchaseController.cs
@@ -73,7 +73,7 @@ namespace API_ClayTrack.Controllers
             return Ok();
         }*/
 
-        [HttpPost]
+        /*[HttpPost]
         [Route("Add")]
         public async Task<ActionResult> AddPurchase([FromBody] CatPurchase purchase)
         {
@@ -102,7 +102,92 @@ namespace API_ClayTrack.Controllers
                 .FirstOrDefault(e => e.fkUser == userId);
 
             return employee;
+        }*/
+
+        [HttpPost]
+        [Route("Add")]
+        public async Task<ActionResult> AddPurchase([FromBody] CatPurchase purchase)
+        {
+            string userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            var employee = GetEmployeeFromDatabase(userId);
+
+            if (employee != null)
+            {
+                purchase.fkCatEmployee = employee.idCatEmployee;
+
+                dbContext.Add(purchase);
+                await dbContext.SaveChangesAsync();
+
+                // Actualiza los totales
+                await UpdateTotalPurchase();
+                await UpdateTotalRawMaterial(purchase.idCatPurchase);
+
+                return Ok();
+            }
+            else
+            {
+                return NotFound("Employee not found.");
+            }
         }
 
+        private CatEmployee GetEmployeeFromDatabase(string userId)
+        {
+            var employee = dbContext.CatEmployee
+                .Include(e => e.Person)
+                .FirstOrDefault(e => e.fkUser == userId);
+
+            return employee;
+        }
+
+        private async Task UpdateTotalRawMaterial(int purchaseId)
+        {
+            var subquery = dbContext.DetailPurchase
+                .Where(dp => dp.fkCatPurchase == purchaseId)
+                .GroupBy(dp => dp.fkCatRawMaterial)
+                .Select(g => new
+                {
+                    fkCatRawMaterial = g.Key,
+                    sum_quantity = g.Sum(dp => dp.quantity)
+                });
+
+            var rawMaterialToUpdate = await dbContext.CatRawMaterial
+                .Join(subquery,
+                    rawMaterial => rawMaterial.idCatRawMaterial,
+                    sub => sub.fkCatRawMaterial,
+                    (rawMaterial, sub) => new { RawMaterial = rawMaterial, Sub = sub })
+                .ToListAsync();
+
+            foreach (var rawMaterialInfo in rawMaterialToUpdate)
+            {
+                rawMaterialInfo.RawMaterial.quantityWarehouse = rawMaterialInfo.Sub.sum_quantity;
+            }
+
+            await dbContext.SaveChangesAsync();
+        }
+
+        private async Task UpdateTotalPurchase()
+        {
+            var subquery = dbContext.DetailPurchase
+                .GroupBy(dp => dp.fkCatPurchase)
+                .Select(g => new
+                {
+                    fkCatPurchase = g.Key,
+                    sum_quantity = g.Sum(dp => dp.price)
+                });
+
+            var purchasesToUpdate = await dbContext.CatPurchase
+                .Join(subquery,
+                    purchase => purchase.idCatPurchase,
+                    sub => sub.fkCatPurchase,
+                    (purchase, sub) => new { Purchase = purchase, Sub = sub })
+                .ToListAsync();
+
+            foreach (var purchaseInfo in purchasesToUpdate)
+            {
+                purchaseInfo.Purchase.total = purchaseInfo.Sub.sum_quantity;
+            }
+
+            await dbContext.SaveChangesAsync();
+        }
     }
 }
